09ef052bf3fbbe3023c3f3414528048d5108b8de
Markus <mschichtl@gmail.com>
2020-02-23 21:00:29 +0100

    refactoring: added base class Component to pipe

--- pipe.py
+++ pipe.py
@@ -1,49 +1,13 @@
-class Pipe:
+from component import Component
 
-    # variable initiation
-    target = None
-    value = 0
-    speed = 1
 
-    def get_target(self):
-        return self.target
-
-    def set_target(self, target):
-        self.target = target
-
-    def get_value(self):
-        return self.value
-
-    def set_value(self, value):
-        self.value = value
-
-    def get_speed(self):
-        return self.speed
-
-    def set_speed(self, speed):
-        self.speed = speed
+class Pipe(Component):
 
     # init method
-    def __init__(self, target=None, speed=1):
+    def __init__(self, target: Component = None, value: int = 0, speed: int = 1):
         self.set_target(target)
+        self.set_value(value)
         self.set_speed(speed)
-
-    # custom methods
-    def add_value(self, value):
-        self.value += value
-
-    def execute(self):
-        if self.get_target() is not None\
-                and self.get_value() > 0:
-            if self.get_value() >= self.get_speed():
-                self.get_target().add_value(self.get_speed())
-                self.add_value(-self.get_speed())
-            else:
-                self.get_target().add_value(self.get_value())
-                self.add_value(-self.get_value())
 
     def to_text(self):
         return '--' + str(self.get_value()) + '--'
-
-    def update(self):
-        self.execute()
