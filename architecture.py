15a7d6e103171295ea76c25d01c4cf90482b5566
Markus <mschichtl@gmail.com>
2020-02-23 17:52:52 +0100

    drafted base elements

---
+++ architecture.py
@@ -0,0 +1,29 @@
+import pipe
+import filter
+
+
+class Architecture:
+
+    start_node = None
+
+    def get_start_node(self):
+        return self.start_node
+
+    def set_start_node(self, start_node):
+        self.start_node = start_node
+
+    def __init__(self, start_node=None):
+        self.set_start_node(start_node)
+
+    def render(self):
+        display_string = ""
+        curr_node = self.get_start_node()
+
+        while curr_node is not None:
+            display_string += str(curr_node.get_value())
+            curr_node = curr_node.get_target()
+
+        print(display_string)
+
+
+
---
+++ filter.py
@@ -0,0 +1,32 @@
+class Filter:
+
+    target = None
+    value = 0
+
+    def __init__(self, target):
+        self.set_target(target)
+
+    def get_target(self):
+        return self.target
+
+    def set_target(self, target):
+        self.target = target
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def add_value(self, value):
+        self.value += value
+
+    def push(self, value=1):
+        self.add_value(value)
+
+    def pop(self, value=1):
+        self.target.push(value)
+        self.add_value(-value)
+
+    def render(self):
+        print('(' + self.get_value() + ')')
