ba0e8358a96b59b457a13d86efd8c0f9df945544
Markus <mschichtl@gmail.com>
2020-02-23 20:57:45 +0100

    refactoring: implemented base class Component and used it for filter

--- component.py
+++ component.py
@@ -1,49 +1,52 @@
-class Pipe:
+from abc import ABCMeta, abstractmethod
+
+
+class Component(metaclass=ABCMeta):
 
     # variable initiation
     target = None
     value = 0
     speed = 1
 
+    # getter and setter methods
+    def set_target(self, target: object = None):
+        self.target = target
+
     def get_target(self):
         return self.target
 
-    def set_target(self, target):
-        self.target = target
+    def set_value(self, value: int = 0):
+        self.value = value
 
     def get_value(self):
         return self.value
 
-    def set_value(self, value):
-        self.value = value
+    def set_speed(self, speed: int = 1):
+        self.speed = speed
 
     def get_speed(self):
         return self.speed
 
-    def set_speed(self, speed):
-        self.speed = speed
+    def add_value(self, value: int = 1):
+        self.value += value
 
     # init method
-    def __init__(self, target=None, speed=1):
+    def __init__(self, target: object = None, value: int = 0, speed: int = 1):
         self.set_target(target)
+        self.set_value(value)
         self.set_speed(speed)
 
     # custom methods
-    def add_value(self, value):
-        self.value += value
-
     def execute(self):
         if self.get_target() is not None\
                 and self.get_value() > 0:
             if self.get_value() >= self.get_speed():
                 self.get_target().add_value(self.get_speed())
                 self.add_value(-self.get_speed())
-            else:
-                self.get_target().add_value(self.get_value())
-                self.add_value(-self.get_value())
 
+    @abstractmethod
     def to_text(self):
-        return '--' + str(self.get_value()) + '--'
+        pass
 
     def update(self):
         self.execute()
--- filter.py
+++ filter.py
@@ -1,47 +1,10 @@
-class Filter:
+from component import Component
 
-    # variable initiation
-    target = None
-    value = 0
-    speed = 1
 
-    # getter and setter methods
-    def get_target(self):
-        return self.target
+class Filter(Component):
 
-    def set_target(self, target):
-        self.target = target
-
-    def get_value(self):
-        return self.value
-
-    def set_value(self, value):
-        self.value = value
-
-    def get_speed(self):
-        return self.speed
-
-    def set_speed(self, speed):
-        self.speed = speed
-
-    # init method
-    def __init__(self, target=None, speed=1):
-        self.set_target(target)
-        self.set_speed(speed)
-
-    # custom methods
-    def add_value(self, value):
-        self.value += value
-
-    def execute(self):
-        if self.get_target() is not None\
-                and self.get_value() > 0:
-            if self.get_value() >= self.get_speed():
-                self.get_target().add_value(self.get_speed())
-                self.add_value(-self.get_speed())
+    def __init__(self, target: Component = None, value:int = 0, speed:int = 1):
+        Component.__init__(self, value, speed)
 
     def to_text(self):
         return '(' + str(self.get_value()) + ')'
-
-    def update(self):
-        self.execute()
